02efc0e6c6ab97c6a2c2e813b6d6c9f25d46f08b
davidmz <david.mazvovsky@gmail.com>
2012-12-08 15:46:32 +0200

    Headers

---
+++ SpeedyMailerTestingApplications/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Mail;
+
+namespace SpeedyMailerTestingApplications
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = new MailMessage("david@xomixinc.com", "davidm@delver.com");
+            message.Body = "yeah";
+            message.Subject = "cool";
+            message.Headers.Add("Speedy-Creaive=Id", "creative/1");
+
+            var client = new SmtpClient();
+            client.Send(message);
+        }
+    }
+}
